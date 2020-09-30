c050116bde223b816eb69f13a490954e12dea3b0
arlukens20 <Drew4818@gmail.com>
2020-09-30 17:29:21 -0400

    fixed Syntax on LocalHost Url

--- src/app/components/file-upload/file-upload.component.ts
+++ src/app/components/file-upload/file-upload.component.ts
@@ -8,7 +8,7 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./file-upload.component.scss']
 })
 export class FileUploadComponent implements OnInit {
-  private Base_Url = "http://localhost:8086/Docutest";
+  private Base_Url = "http://localhost:8083/Docutest";
   public uploadForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
@@ -29,7 +29,7 @@ export class FileUploadComponent implements OnInit {
   onSubmit() {
     const formData = new FormData();
     formData.append('file', this.uploadForm.get('swaggerFile').value);
-    this.http.post<any>(this.Base_Url, formData).subscribe(
+    this.http.post<any>(this.Base_Url + '/upload', formData).subscribe(
       (res) => console.log(res),
       (err) => console.log(err));
   }
